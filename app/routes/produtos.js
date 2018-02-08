d3101e5e42fc1df7ef16d5bac3d529ea3a9ebc59
Gustavo Barbosa <gustavobarbosabarreto@hotmail.com>
2018-02-08 14:28:53 -0200

    antes de adicionar o express-load

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,24 +1,15 @@
+var dbConnection = require(`../infra/connectionFactory`);
+
 module.exports = function(app){
   app.get('/produtos',function(request,response){
-    var mysql = require(`mysql`);
-    var connection = mysql.createConnection({
-      host:`localhost`,
-      user:`root`,
-      password:`root`,
-      database:`casaCodigo`
-    });
+    var connection = dbConnection();
 
     connection.query(`SELECT * FROM livros`,function(error,results){
-      response.setHeader("Content-Type", "application/json");
-      response.send(results);
+    response.setHeader("Content-Type", "application/json");
+    response.send(results);
     });
 
     //response.render("produtos/lista");
-    requestURL(`/produtos`);
     connection.end();
   });
-}
-
-function requestURL(endpoint){
-  console.log("request in:"+endpoint);
 }
